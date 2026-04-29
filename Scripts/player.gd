5dfa9d9fd9eaa13b47f729f749bda70e5ea89db7
EmySchmidt <167031017+EmySchmidt@users.noreply.github.com>
2026-04-28 21:45:48 -0300

    fisica player
    atualização dos movimentos jogador

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -14,16 +14,16 @@ func _process(delta: float) -> void:
 	position += movement * delta * movement_speed
 
 func _input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("right"):
+	if Input.is_action_pressed("right"):
 		movement = Vector2.RIGHT
 		sprite.play("walk_right")
-	elif Input.is_action_just_pressed("left"):
+	elif Input.is_action_pressed("left"):
 		movement = Vector2.LEFT
 		sprite.play("walk_left")
-	elif Input.is_action_just_pressed("up"):
+	elif Input.is_action_pressed("up"):
 		movement = Vector2.UP
 		sprite.play("walk_up")
-	elif Input.is_action_just_pressed("down"):
+	elif Input.is_action_pressed("down"):
 		movement = Vector2.DOWN
 		sprite.play("walk_down")
 	else:
