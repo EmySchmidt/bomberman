34688dbbbdf0f1390f5e47f4c37d2d721ed796aa
maribrincadeirinhas-max <marifloresp07@gmail.com>
2026-05-03 10:27:18 -0300

    correção de código
    correção

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,32 +5,16 @@ class_name Player
 var movement: Vector2 = Vector2.ZERO
 @export var movement_speed: float = 75
 @onready  var sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var raycasts:Raycasts = $"../Raycasts"
-
-func _process(delta: float) -> void:
-	
-	
-	
-	position += movement * delta * movement_speed
-	
-@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var raycasts = $Raycasts
 @onready var bomb_placement_system: BombPlacementSystem = $BombPlacementSystem
 var max_bomb = 1
 
-func _ready() -> void:
-	pass 
-
 func _process(delta: float) -> void:
-	
 	var collisions = raycasts.check_collisions()
 	if collisions.has(movement):
 		return
 	position += movement * delta * movement_speed
 	
-	
-
->>>>>>> b2f0b7f02863757c8c0071786f24dd58541776ea
 func _input(event: InputEvent) -> void:
 	if Input.is_action_pressed("right"):
 		movement = Vector2.RIGHT
